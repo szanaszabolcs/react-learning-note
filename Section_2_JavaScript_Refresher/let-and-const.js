891de516173ac949bd901b11f0d6e5ee1ac9c5a8
Szabols Szana <szana.szabolcs@gmail.com>
2021-12-28 14:49:47 +0100

    12. Understanding "let" and "const"

---
+++ Section_2_JavaScript_Refresher/let-and-const.js
@@ -0,0 +1,15 @@
+var myName = "Szabolcs";
+console.log(myName);
+
+myName = "Mark";
+console.log(myName);
+
+let myNameWithLet = "Szabolcs";
+console.log(myNameWithLet);
+
+myNameWithLet = "Mark";
+console.log(myNameWithLet);
+
+const myNameWithConst = "Szabolcs";
+console.log(myNameWithConst);
+// myNameWithConst = "Mark";
